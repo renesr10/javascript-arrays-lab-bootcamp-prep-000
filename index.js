f6b6baa419af48a90db037ab169f8fc83d944e96
Rene Santana <renesantana10@gmail.com>
2020-01-22 22:37:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function appendKitten(name) {
 }
 function prependKitten(name) {
   
-  return newArray
+  return [name, . . .]
 }
 function removeLastKitten(name) {
   var newArray = kittens.slice(name)
